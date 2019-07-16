2cd3cb6c537ed5a204d1422b5da6d0ef55f11c34
Aidi Chen <50345185+ididChan@users.noreply.github.com>
2019-07-16 17:05:57 +0800

    Add files via upload

--- piyao_info/piyao_dynamic.py
+++ piyao_info/piyao_dynamic.py
@@ -1,62 +1,109 @@
 import json
 import urllib
-from urllib import request
-import jsonpath
-from bs4 import BeautifulSoup
+from jsonpath import jsonpath
+import requests
 
 class CrawingInfo():
-    def __init__(self, pgnum, callback_num):
+
+    def __init__(self, pgnum, callback_num, url):
         self.pgnum = pgnum
         self.callback_num=callback_num
-        self.headers={
+        self.headers = {
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-            'Referer': 'http://www.xinhuanet.com/food/sppy/qwpy.htm',
-            'Request URL': 'http://qc.wa.news.cn/nodeart/list?nid=11148362&pgnum={}&cnt=10&tp=1&orderby=1?callback=jQuery112401431122865743193_1563187623741&_{}'.format(self.pgnum,self.callback_num),
-            # 'Content-Encoding':'UTF-8'
         }
+        self.url = "http://qc.wa.news.cn/nodeart/list?nid=11148362&pgnum={}&cnt=10&tp=1&orderby=1?callback=jQuery112401431122865743193_1563187623741&_{}".format(self.pgnum,self.callback_num)
 
-    def craw_data(self, pgnum, callback_num):
-        url = "http://qc.wa.news.cn/nodeart/list?nid=11148362&pgnum={}&cnt=10&tp=1&orderby=1?callback=jQuery112401431122865743193_1563187623741&_{}".format(self.pgnum,self.callback_num)
+    def craw_title(self):
 
-        try:
-            with request.urlopen(url,timeout=10) as js:
-                
-                content = js.read()
-                jsobj = json.loads(content)   #将json转化成python对象
+        js = requests.get(self.url,headers=self.headers)
+        content_bin = js.content
+        content_str = content_bin.decode('utf8')
+        content = content_str[42:-1]
+        jsobj = json.loads(content)
+        title = jsonpath(jsobj,'$..Title')
+        js.close()
 
-                title = jsonpath.jsonpath(jsobj,'$..Title')
-                linkurl = jsonpath.jsonpath(jsobj,'$..LinkUrl')
-                pubtime = jsonpath.jsonpath(jsobj,'$..PubTime')
-                editor = jsonpath.jsonpath(jsobj,'$..Editor')
-                sourcename = jsonpath.jsonpath(jsobj,'$..SourceName')
-
-                yaoyan_info = zip(title, linkurl, pubtime, editor, sourcename)
-        except:
-            content = None
-            print('crawl data exception.')
-
-        print(yaoyan_info) 
+        return title
     
-    # def get_content(self, yaoyan_info):
+    def craw_linkurl(self):
 
-    #     url_list = yaoyan_info[1]
+        js = requests.get(self.url,headers=self.headers)
+        content_bin = js.content
+        content_str = content_bin.decode('utf8')
+        content = content_str[42:-1]
+        jsobj = json.loads(content)
+        linkurl = jsonpath(jsobj,'$..LinkUrl')
+        js.close()
 
-    #     for url in url_list:
+        return linkurl
+
+    def craw_pubtime(self):
+
+        js = requests.get(self.url,headers=self.headers)
+        content_bin = js.content
+        content_str = content_bin.decode('utf8')
+        content = content_str[42:-1]
+        jsobj = json.loads(content)
+        pubtime = jsonpath(jsobj,'$..PubTime')
+        js.close()
+
+        return pubtime
+
+    def craw_editor(self):
+
+        js = requests.get(self.url,headers=self.headers)
+        content_bin = js.content
+        content_str = content_bin.decode('utf8')
+        content = content_str[42:-1]
+        jsobj = json.loads(content)
+        editor = jsonpath(jsobj,'$..Editor')
+        js.close()
+
+        return editor
+            
+    def craw_sourcename(self):
+
+        js = requests.get(self.url,headers=self.headers)
+        content_bin = js.content
+        content_str = content_bin.decode('utf8')
+        content = content_str[42:-1]
+        jsobj = json.loads(content)
+        sourcename = jsonpath(jsobj,'$..SourceName')
+        js.close()
+
+        return sourcename
 
     def go(self):
 
-        pgnum = 1
-        callback_num = 1563187623742
-
         while True:
-            if(pgnum>4):
+            if(self.pgnum>4):
                 return
-            self.craw_data(pgnum,callback_num)
-            pgnum+=1
-            callback_num+=1
+            
+            self.pgnum+=1
+            self.callback_num+=1
 
 if __name__ == "__main__":
-    CrawingInfo(1, 1563187623742)
+    i = 1
+    a = CrawingInfo(1, 1563187623742,"")
+    while i<5:
+        title=a.craw_title()
+        sourcename = a.craw_sourcename()
+        pubtime = a.craw_pubtime()
+        url = a.craw_linkurl()
+        editor = a.craw_editor()
+        a.go
+
+        yaoyan_list = zip(title, sourcename, pubtime, url, editor)
+        yaoyan_list = [j for j in yaoyan_list]
+
+        for item in yaoyan_list:
+            print('Title:', item[0])
+            print('PubTime:', item[2])
+            print('Source:', item[1])
+            print('Editor',item[4])
+            print('ContentURL:',item[3])
+            print('\n')
+
     pass
 
 
